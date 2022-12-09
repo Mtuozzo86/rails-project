5dee4f9cda44c243232cc657c3e7618e3504bf4d
Mtuozzo86 <mtuozzo86@gmail.com>
2022-12-09 17:48:48 -0500

    test again for deploy

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -7,6 +7,6 @@ npm install --prefix client && npm run build --prefix client
 cp -a client/build/. public/
 
 bundle install
-bundle exec rake assets:precompile
-bundle exec rake assets:clean
+# bundle exec rake assets:precompile
+# bundle exec rake assets:clean
 bundle exec rake db:migrate
